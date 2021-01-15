663c842b5ecf03847f6330964f68c703e42f51e0
taha <taha.maatof@gmail.com>
2021-01-15 02:51:55 +0100

    updated requirement

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -180,3 +181,5 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'shop.authentication.EmailAuthBackend',
 ]
+
+django_heroku.settings(locals())

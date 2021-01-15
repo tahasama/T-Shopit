3119bb6f68179b8f2e0ecee1835ec3b004a9974d
taha <taha.maatof@gmail.com>
2021-01-15 03:48:29 +0100

    main

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -19,8 +19,8 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env(SECRET_KEY=str,)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# env = environ.Env(SECRET_KEY=str,)
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 

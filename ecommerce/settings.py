0b2e35d277769aa16b0fe5a3e3f9af836220aa5e
taha <taha.maatof@gmail.com>
2021-01-15 02:30:30 +0100

    push to github

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,21 +11,29 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(SECRET_KEY=str,)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(u-d3^8gchy68%g3-hgp4+zse-9uaf@_m++w^=pi3#co#qq2xc'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+		
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DJANGO_DEBUG')
+		
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS')
 
 
 # Application definition
@@ -81,10 +89,19 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
     }
 }
 
@@ -154,8 +171,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'serertei@gmail.com'
-EMAIL_HOST_PASSWORD = 'buvgksgouhdhvysg'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
--- shop/urls.py
+++ shop/urls.py
@@ -3,12 +3,12 @@ from . import views
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    #path('',cache_page(60 * 15)( views.home), name='home'),
-    path('category/<slug:category_slug>', views.home, name='products_by_category'),
-    #path('category/<slug:category_slug>', cache_page(60 * 15)(views.home), name='products_by_category'),
-    path('category/<slug:category_slug>/<slug:product_slug>', views.productPage, name='product_detail'),
-    #path('category/<slug:category_slug>/<slug:product_slug>',cache_page(60 * 15)(views.productPage), name='product_detail'),
+    #path('', views.home, name='home'),
+    path('',cache_page(60 * 15)( views.home), name='home'),
+    #path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>', cache_page(60 * 15)(views.home), name='products_by_category'),
+    #path('category/<slug:category_slug>/<slug:product_slug>', views.productPage, name='product_detail'),
+    path('category/<slug:category_slug>/<slug:product_slug>',cache_page(60 * 15)(views.productPage), name='product_detail'),
     path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
